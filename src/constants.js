5a446c6cba86b30d87bf7562b37bb03b21caac9d
Marina Batina <mbatina.oat@gmail.com>
2023-09-26 15:55:02 +0300

    Introduce setting variables file.

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,4 @@
-AKIAYQMDGBW7TBSHJSNY
+export const S3_BUCKET = 'marina-1';//'meri-rastila';
+export const UPLOAD_FOLDER = 'upload/';//'test';
+export const MAX_FILE_SIZE = 10 * 1024 * 1024;
+export const AWS_REGION = 'eu-central-1';//'eu-north-1';

8e9ceec7e844442f8bb120610d0a9c1a9716e5b1
Marina Batina <mbatina.oat@gmail.com>
2023-09-26 15:57:43 +0300

    Style Home component. Introduce Footer component (WIP).

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,24 @@
-
+import { Link } from 'react-router-dom'; 
 import classes from './Footer.module.css'
 
 const Footer = ()=>{
+    const year = new Date().getFullYear();
+
      return(
-        <div className={classes.footer}>I'm Footer</div>
+        <div className={classes.footer}>
+            <div className={classes.footer_text} >
+                <p >Copyright © {year} Marina Batina.<span className="rights-footer"> &nbsp;All rights reserved &nbsp;&nbsp; |</span> </p>
+                <p >Gif Creator v.0.1.1 &nbsp;&nbsp;|</p>
+            </div> 
+            <div >  
+                <Link to= "https://github.com/Marina-August" className="opacity-50 hover:opacity-75"><img src="/assets/images/github.png"  height="20" width="20"/></Link>
+                <Link to= "https://www.linkedin.com/in/marina-batina/"  className="opacity-50 hover:opacity-75"><img src="/assets/images/linkedin.png" height="20" width="20"/></Link>
+                <Link to= "https://www.codewars.com/users/Marina_August"  className="opacity-50 hover:opacity-75"><img src="https://www.codewars.com/packs/assets/logo.f607a0fb.svg" hight= "25" width = "25"/></Link>  
+                <a href="mailto:mbatina.oat@gmail.com" className="lg:mr-16 md:mr-2 opacity-50 hover:opacity-75">
+                    <img src="/assets/images/gmail.png" alt="Send an email" height="25" width="25"/>
+                </a> 
+            </div> 
+        </div>
      )
 }
 
